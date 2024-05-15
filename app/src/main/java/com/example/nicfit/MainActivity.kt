2bdd32e0bc9af8fca55029a23cdfeb821be2372f
Muhammad Riyadhi <mriyadhi914@gmail.com>
2024-05-15 23:10:14 +0700

    beranda

--- app/src/main/java/com/example/nicfit/MainActivity.kt
+++ app/src/main/java/com/example/nicfit/MainActivity.kt
@@ -10,9 +10,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.nicfit.beranda.beranda
 import com.example.nicfit.daftar.RegisScreen
 import com.example.nicfit.masuk.login
 import com.example.nicfit.navigation.AppNavigation
+import com.example.nicfit.navigation.Screens
 import com.example.nicfit.ui.theme.NicFitTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +23,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             //login()
             //RegisScreen()
-            AppNavigation()
+            //AppNavigation()
+            beranda()
         }
     }
 }
--- app/src/main/java/com/example/nicfit/beranda/beranda.kt
+++ app/src/main/java/com/example/nicfit/beranda/beranda.kt
@@ -1,9 +1,24 @@
 package com.example.nicfit.beranda
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester.Companion.createRefs
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.widget.ConstraintLayout
+import kotlinx.coroutines.NonDisposableHandle.parent
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun beranda() {
-    Text(text = "BERANDA UTAMA")
+   Text(text = "Beranda")
 }
+
+
